ab2df7fa1c44fffa9350980909b160b06877c93c
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-09-26 23:55:52 +0200

    Create IDriver.java

---
+++ src/main/java/com/example/IDriver.java
@@ -0,0 +1,9 @@
+public interface IDriver {
+    void createDatabase(String databaseSchemaPath);
+
+    void setRow(String databaseName, String tableName,String value);
+
+    void getRow(String database, String table, String value);
+
+    void deleteRow(String databaseName, String tableName,String value);
+}
